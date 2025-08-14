6ab06100f907f2edcd7693a584e8aba6750a37d3
Jpzinn654 <juan654.pablo@gmail.com>
2025-08-14 11:16:00 -0300

    feat: add post artifact in main class, fix sharepoint suffix

--- bot.py
+++ bot.py
@@ -17,19 +17,19 @@ def parse_args() -> argparse.Namespace:
         "--environment",
         type=str,
         choices=[settings.CHOICE_MAESTRO, settings.CHOICE_LOCAL],
-        default=settings.CHOICE_MAESTRO,  # Set 'maestro' as the default
+        default=settings.CHOICE_MAESTRO, # Set 'maestro' as the default
         help=settings.HELP_MESSAGE,
     )
-    args, _ = parser.parse_known_args()  # Get only required arguments
+    args, _ = parser.parse_known_args() # Get only required arguments
 
     return args
 
-
-if __name__ == "__main__":
-    args = parse_args()
-
+def get_bot_runner(args):
+    """
+    Factory method to create the appropriate bot runner based on arguments.
+    """
     if args.environment == settings.CHOICE_MAESTRO:
-        bot_runner = BotRunnerMaestro()
+        return BotRunnerMaestro()
     else:
         if (
             not settings.SERVER_MAESTRO
@@ -39,11 +39,14 @@ if __name__ == "__main__":
             raise ValueError(
                 "SERVER_MAESTRO, LOGIN_MAESTRO, and KEY_MAESTRO must be provided in the .env file"
             )
-
-        bot_runner = BotRunnerLocal(
+        return BotRunnerLocal(
             server=settings.SERVER_MAESTRO,
             login=settings.LOGIN_MAESTRO,
             key=settings.KEY_MAESTRO,
         )
 
+
+if __name__ == "__main__":
+    args = parse_args()
+    bot_runner = get_bot_runner(args)
     bot_runner.run()
